083995bb882cf3e64f47d6be57c9fd73645c0b2f
Jeremy Goh <gohj@visa.com>
2026-05-12 17:21:46 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+WITH TRIP AS(
+
+select
+RIDE_ID,
+RIDEABLE_TYPE,
+DATE(TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE,
+START_STATION_ID,
+END_STATION_ID,
+MEMBER_CASUAL,
+TIMESTAMPDIFF(SECOND, TO_TIMESTAMP(STARTED_AT), TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION_SECONDS
+from
+{{ source('demo', 'bike') }}
+where RIDE_ID != 'ride_id'
+
+)
+
+select
+*
+from 
+TRIP
